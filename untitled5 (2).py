bfb18e0d9e2c548deaa0c7d1c5b773d163385669
candela <cramallo@uca.edu.ar>
2025-04-03 19:16:23 -0300

    Add files via upload

---
+++ untitled5 (2).py
@@ -0,0 +1,16 @@
+
+frutas = ["naranja", "pera", "uva ","manzana"]
+for fruta  in frutas:
+    print("fruta")
+    
+    
+while #repite un bloque mientras una condicion sea verdadera  
+
+contador=0
+while contador < 5:
+    print(contador)
+    contador +=1
+    if contador == 5:
+        break
+    
+    
