e765dff05b25871174df14720f1b11bdbd6ae6aa
candelaramallo <cramallo@uca.edu.ar>
2025-04-03 18:37:23 -0300

    Add files via upload

---
+++ untitled3.py
@@ -0,0 +1,11 @@
+calificacion = 60
+
+if calificacion >= 90:
+    print (" exelente")
+    
+elif calificacion >= 80:
+    print (" muy bueno")
+
+elif calificacion <= 70:
+    print (" necesita mejorar ")
+
---
+++ untitled4.py
@@ -0,0 +1,12 @@
+a = input ("ingrese valor") 
+b = input ("ingresa segundo valor")
+if a<b:
+    print ("el primer valor es mayor")
+    
+elif a>b:
+    print ("el segundo valor es mayor") 
+
+elif a==b:
+    print ("los valores son iguales")
+    
+reversed: (" dar vuelta los valores de a y b")
